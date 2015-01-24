f3e5e85ad5c92df35f69c5fccc66ea79f1bf26a9
Brendon Wai <brendonwai@dhcp-v037-075.mobile.uci.edu>
2015-01-24 11:25:01 -0800

    Added in player sprite and rotation

--- Hit or Run/Assets/Scripts/Player.cs
+++ Hit or Run/Assets/Scripts/Player.cs
@@ -5,30 +5,49 @@ public class Player : MonoBehaviour
 {
 	public Rigidbody Bullet;
 	public Vector3 playerPosition;
+	public float speed=10;
+	private Animator anim;
 	// Use this for initialization
 	void Start () 
 	{
-	
+		anim = GetComponent<Animator> ();
 	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		CharacterController controller = GetComponent<CharacterController>();
-		playerPosition = transform.position;
 
+	//Use FixedUpdate for frame-independent physics activities
+	void FixedUpdate(){
+		//alternative character control
+		//make your life easier with this instead of setting each of wasd keys
+		rigidbody2D.velocity = new Vector2 (Input.GetAxis ("Horizontal")*speed, Input.GetAxis ("Vertical")*speed);
+
+		/*
+		playerPosition = transform.position;
 		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 		RaycastHit hit = new RaycastHit ();
 
 		if (Physics.Raycast (ray, out hit, 100)) 
 		{
-
 			Vector3 hitPoint = hit.point;
 			Vector3 targetDir = hitPoint - transform.position;
+			
 			float angle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg;
 			transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 		}
+		*/
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if(rigidbody2D.velocity!=Vector2.zero)
+			anim.SetBool("walk",true);
+		
+		else
+			anim.SetBool("walk",false);
+
+
 		//Character Controls
+		/*
 		if (Input.GetKey (KeyCode.A))
 			controller.Move (Vector3.left * Time.deltaTime * 5);
 		if (Input.GetKey (KeyCode.D))
@@ -37,6 +56,7 @@ public class Player : MonoBehaviour
 			controller.Move (Vector3.up * Time.deltaTime * 5);
 		if (Input.GetKey (KeyCode.S))
 			controller.Move (Vector3.down * Time.deltaTime * 5);
+			*/
 		//transform.RotateAround (transform.position, Vector3.forward, .1f);
 
 		//Create bullet with Left Click
