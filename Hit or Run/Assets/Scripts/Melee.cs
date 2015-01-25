1b74dc6de2197d9f61dbe437c2dc1933d8b96bb3
Khai Nguyen <khai.b.nguyen@gmail.com>
2015-01-24 20:17:21 -0800

    Added in Melee script

---
+++ Hit or Run/Assets/Scripts/Melee.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Melee : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        //Destroy (this.gameObject);
+        if (coll.tag == "Enemy" || coll.tag == "Boss")
+        {
+            coll.GetComponent<EnemyAI>().TakeDamage();
+            Debug.Log("Enemy Hit");
+        }
+    }
+}
